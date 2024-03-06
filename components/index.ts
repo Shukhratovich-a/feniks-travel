cb71f8fea70685ad3574d08c12ba14a77ac63b50
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-06 12:48:29 +0500

    refactor: export created components

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,12 @@
 export * from "./ui/Container/Container.component";
-export * from "./ui/Input/Input.component";
+export * from "./ui/Card/Card.component";
+export * from "./ui/Slider/Slider.component";
+export * from "./ui/Rating/Rating.component";
+export * from "./ui/Burger/Burger.component";
+export * from "./ui/Logo/Logo.component";
 
 export * from "./Language/Language.component";
 export * from "./Currency/Currency.component";
+
+export * from "./TourCard/TourCard.component";
+export * from "./DestinationCard/DestinationCard.component";
