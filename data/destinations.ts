8f62c04bff581f83f8072b02abc9672e82b1bbd6
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-06 12:58:10 +0500

    feat: create mock data

---
+++ data/destinations.ts
@@ -0,0 +1,34 @@
+import TourImage from "@/images/hero-background.jpg";
+
+export const destinations = [
+  {
+    id: 1,
+    title: "Egypt",
+    image: TourImage,
+  },
+  {
+    id: 2,
+    title: "Egypt",
+    image: TourImage,
+  },
+  {
+    id: 3,
+    title: "Egypt",
+    image: TourImage,
+  },
+  {
+    id: 4,
+    title: "Egypt",
+    image: TourImage,
+  },
+  {
+    id: 5,
+    title: "Egypt",
+    image: TourImage,
+  },
+  {
+    id: 6,
+    title: "Egypt",
+    image: TourImage,
+  },
+];
---
+++ data/tours.ts
@@ -0,0 +1,82 @@
+import TourImage from "@/images/hero-background.jpg";
+
+export const tours = [
+  {
+    id: 1,
+    title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
+    image: TourImage,
+    destination: {
+      id: 1,
+      title: "Egypt",
+      image: TourImage,
+    },
+    rating: 5,
+    duration: 7,
+    price: 180,
+  },
+  {
+    id: 2,
+    title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
+    image: TourImage,
+    destination: {
+      id: 1,
+      title: "Egypt",
+      image: TourImage,
+    },
+    rating: 3,
+    duration: 7,
+    price: 180,
+  },
+  {
+    id: 3,
+    title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
+    image: TourImage,
+    destination: {
+      id: 1,
+      title: "Egypt",
+      image: TourImage,
+    },
+    rating: 5,
+    duration: 7,
+    price: 180,
+  },
+  {
+    id: 4,
+    title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
+    image: TourImage,
+    destination: {
+      id: 1,
+      title: "Egypt",
+      image: TourImage,
+    },
+    rating: 5,
+    duration: 7,
+    price: 180,
+  },
+  {
+    id: 5,
+    title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
+    image: TourImage,
+    destination: {
+      id: 1,
+      title: "Egypt",
+      image: TourImage,
+    },
+    rating: 4,
+    duration: 7,
+    price: 180,
+  },
+  {
+    id: 6,
+    title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
+    image: TourImage,
+    destination: {
+      id: 1,
+      title: "Egypt",
+      image: TourImage,
+    },
+    rating: 5,
+    duration: 7,
+    price: 180,
+  },
+];
