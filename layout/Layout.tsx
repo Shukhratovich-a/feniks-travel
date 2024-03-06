d0fb74995ee597e9490684bfbad3d18b3bf0fb57
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-06 12:53:28 +0500

    refactor: connect sidebar

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,18 +1,23 @@
 import { FC } from "react";
-import { Montserrat, Inter } from "next/font/google";
+import dynamic from "next/dynamic";
 import cn from "classnames";
 
 import { LayoutProps } from "./Layout.props";
 
+import { SideBarProvider } from "@/contexts/sidebar.context";
+
 import { Footer } from "./Footer/Footer.component";
-import { Header } from "./Header/Header.component";
 
 import styles from "./Layout.module.scss";
+
+const Header = dynamic(() => import("./Header/Header.component"), { ssr: false });
+const Sidebar = dynamic(() => import("./Sidebar/Sidebar.component"), { ssr: false });
 
 export const Layout: FC<LayoutProps> = ({ children, className, ...props }) => {
   return (
     <div className={cn(styles.layout)} {...props}>
       <Header className={cn(styles.layout__header)} />
+      <Sidebar className={cn(styles.layout__sidebar)} />
 
       <main className={cn(styles.layout__main)}>{children}</main>
 
@@ -24,9 +29,11 @@ export const Layout: FC<LayoutProps> = ({ children, className, ...props }) => {
 export const withLayout = <T extends Record<string, unknown>>(Component: FC<T>) => {
   return function withLayoutComponent(props: T): JSX.Element {
     return (
-      <Layout>
-        <Component {...props} />
-      </Layout>
+      <SideBarProvider isOpen={false}>
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      </SideBarProvider>
     );
   };
 };
