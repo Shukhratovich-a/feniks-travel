0d35840cadbabcc46c83377277ec155b8cffa169
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-06 12:58:55 +0500

    refactor: remove old data and connect mock data

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,129 +7,16 @@ import { IDestination } from "@/interfaces/destination.interface";
 
 import { withLayout } from "@/layout/Layout";
 
-import { HomeView } from "@/views";
+import { tours } from "@/data/tours";
+import { destinations } from "@/data/destinations";
 
-import TourImage from "@/images/hero-background.jpg";
+import { HomeView } from "@/views";
 
 const HomePage: FC<HomePageProps> = ({ tours, destinations }) => {
   return <HomeView tours={tours} destinations={destinations} />;
 };
 
 export const getStaticProps: GetStaticProps<HomePageProps> = async ({ locale }) => {
-  const tours: ITour[] = [
-    {
-      id: 1,
-      title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
-      image: TourImage,
-      destination: {
-        id: 1,
-        title: "Egypt",
-        image: TourImage,
-      },
-      rating: 5,
-      duration: 7,
-      price: 180,
-    },
-    {
-      id: 2,
-      title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
-      image: TourImage,
-      destination: {
-        id: 1,
-        title: "Egypt",
-        image: TourImage,
-      },
-      rating: 3,
-      duration: 7,
-      price: 180,
-    },
-    {
-      id: 3,
-      title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
-      image: TourImage,
-      destination: {
-        id: 1,
-        title: "Egypt",
-        image: TourImage,
-      },
-      rating: 5,
-      duration: 7,
-      price: 180,
-    },
-    {
-      id: 4,
-      title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
-      image: TourImage,
-      destination: {
-        id: 1,
-        title: "Egypt",
-        image: TourImage,
-      },
-      rating: 5,
-      duration: 7,
-      price: 180,
-    },
-    {
-      id: 5,
-      title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
-      image: TourImage,
-      destination: {
-        id: 1,
-        title: "Egypt",
-        image: TourImage,
-      },
-      rating: 4,
-      duration: 7,
-      price: 180,
-    },
-    {
-      id: 6,
-      title: "Centipede Tour - Guided Arizona Desert Tour by ATV",
-      image: TourImage,
-      destination: {
-        id: 1,
-        title: "Egypt",
-        image: TourImage,
-      },
-      rating: 5,
-      duration: 7,
-      price: 180,
-    },
-  ];
-
-  const destinations = [
-    {
-      id: 1,
-      title: "Egypt",
-      image: TourImage,
-    },
-    {
-      id: 2,
-      title: "Egypt",
-      image: TourImage,
-    },
-    {
-      id: 3,
-      title: "Egypt",
-      image: TourImage,
-    },
-    {
-      id: 4,
-      title: "Egypt",
-      image: TourImage,
-    },
-    {
-      id: 5,
-      title: "Egypt",
-      image: TourImage,
-    },
-    {
-      id: 6,
-      title: "Egypt",
-      image: TourImage,
-    },
-  ];
-
   return {
     props: {
       tours,
