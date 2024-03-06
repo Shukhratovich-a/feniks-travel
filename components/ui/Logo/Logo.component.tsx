4d456d87336a7afcd64d13f511410e8a4dcb1b53
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-06 12:45:13 +0500

    feat: create logo component

---
+++ components/ui/Logo/Logo.component.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import cn from "classnames";
+
+import { LogoProps } from "./Logo.props";
+
+import styles from "./Logo.module.scss";
+
+export const Logo: FC<LogoProps> = ({ className, ...props }) => {
+  return (
+    <div className={cn(styles.logo, className)} {...props}>
+      Logo
+    </div>
+  );
+};
