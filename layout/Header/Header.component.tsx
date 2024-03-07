94eca2ae1e436a5a83caba7227bfcebe43f9a80e
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-07 18:36:58 +0500

    refactor: add scroll styles

--- layout/Header/Header.component.tsx
+++ layout/Header/Header.component.tsx
@@ -1,4 +1,4 @@
-import { FC, useContext } from "react";
+import { FC, useCallback, useContext, useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import cn from "classnames";
 
@@ -12,16 +12,52 @@ import { Navbar } from "../Nav/Nav.component";
 import styles from "./Header.module.scss";
 
 export const Header: FC<HeaderProps> = ({ className, ...props }) => {
-  const isTablet = useMediaQuery({ maxWidth: 850 });
+  const isBrowser = typeof window !== undefined;
 
+  const isDesktop = useMediaQuery({ maxWidth: 1200 });
+  const isTablet = useMediaQuery({ maxWidth: 850 });
+  const isMobile = useMediaQuery({ maxWidth: 500 });
   const { isOpen, setIsOpen } = useContext(SideBarContext);
+
+  const scrollThreshold = isMobile ? 70 : isTablet ? 80 : isDesktop ? 100 : 100;
+
+  const [scrolled, setScrolled] = useState(false);
 
   const menuController = () => {
     if (setIsOpen) setIsOpen(!isOpen);
   };
 
+  const handleScroll = useCallback(() => {
+    if (!isBrowser) return;
+
+    const isScrolled = window.scrollY > scrollThreshold;
+
+    setScrolled(isScrolled);
+  }, [isBrowser, scrollThreshold]);
+
+  useEffect(() => {
+    if (!isBrowser) return;
+
+    return handleScroll();
+  }, [handleScroll, isBrowser]);
+
+  useEffect(() => {
+    if (!isBrowser) return;
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll, isBrowser]);
+
   return (
-    <header className={cn(styles.header, className)} {...props}>
+    <header
+      className={cn(styles.header, className, {
+        [styles["header--scroll"]]: scrolled,
+      })}
+      {...props}
+    >
       <Container className={cn(styles.header__container)}>
         <div className={cn(styles.header__inner)}>
           {!isTablet && (
