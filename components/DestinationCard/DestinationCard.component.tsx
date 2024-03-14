869a67dae22cf4e183c67e9c719994e05b026e53
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-14 20:36:34 +0500

    refactor: add translate

--- components/DestinationCard/DestinationCard.component.tsx
+++ components/DestinationCard/DestinationCard.component.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import Image from "next/image";
+import { useTranslation } from "next-i18next";
 import cn from "classnames";
 
 import { DestinationCardProps } from "./DestinationCard.props";
@@ -9,13 +10,15 @@ import { Card } from "@/components";
 import styles from "./DestinationCard.module.scss";
 
 export const DestinationCard: FC<DestinationCardProps> = ({ className, destination, ...props }) => {
+  const { t } = useTranslation("destinations");
+
   const { title, image } = destination;
 
   return (
     <Card className={cn(styles.card, className)} {...props}>
       <Image className={cn(styles.card__image)} src={image} alt={title} sizes="100%" fill priority />
 
-      <h3 className={cn(styles.card__title)}>{title}</h3>
+      <h3 className={cn(styles.card__title)}>{t(title)}</h3>
     </Card>
   );
 };
