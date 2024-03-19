74309b38c645112b3a05b9128a17ff09965d4815
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-19 11:45:28 +0500

    refactor: export icons

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -3,3 +3,9 @@ export { default as IconLocation } from "./location.svg";
 export { default as IconStar } from "./star.svg";
 export { default as IconBurger } from "./burger.svg";
 export { default as IconClose } from "./close.svg";
+
+export { default as IconPhone } from "./phone.svg";
+export { default as IconMail } from "./mail.svg";
+export { default as IconTime } from "./time.svg";
+export { default as IconAddress } from "./address.svg";
+export { default as IconPerson } from "./person.svg";
