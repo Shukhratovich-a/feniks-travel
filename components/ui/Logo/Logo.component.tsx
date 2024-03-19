5625b7bc1e0b08b0d6fb702c7e8c850c53be9e0b
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-19 11:46:10 +0500

    refactor: connect real logo

--- components/ui/Logo/Logo.component.tsx
+++ components/ui/Logo/Logo.component.tsx
@@ -1,14 +1,20 @@
 import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
 import cn from "classnames";
 
 import { LogoProps } from "./Logo.props";
+
+import ImageLogo from "@/images/logo/logo.webp";
 
 import styles from "./Logo.module.scss";
 
 export const Logo: FC<LogoProps> = ({ className, ...props }) => {
   return (
     <div className={cn(styles.logo, className)} {...props}>
-      Feniks Travel
+      <Link href="#hero">
+        <Image className={cn(styles.logo__image, className)} src={ImageLogo} alt="Feniks travel's logo" height={80} />
+      </Link>
     </div>
   );
 };
