5efc54214cd9a7ec04794f242e4b045062752508
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-22 13:55:20 +0500

    feat: create tour api

---
+++ api/tour.api.ts
@@ -0,0 +1,14 @@
+import queryString from "query-string";
+
+import axios from "./axios";
+
+import { ITour } from "@/interfaces/tour.interface";
+import { IGetListOptions } from "@/interfaces/request.interface";
+
+import { API } from "@/helpers/api.helper";
+
+export const getList = (options?: IGetListOptions) => {
+  const query = queryString.stringifyUrl({ url: API.tour.getList, query: { ...options } });
+
+  return axios.get<ITour[]>(query);
+};
