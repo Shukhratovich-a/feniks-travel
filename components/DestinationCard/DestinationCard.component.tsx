a611a58b3f4a5388500ef43b7db9b2aa900fa8dc
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-22 15:06:10 +0500

    refactor: change title to name

--- components/DestinationCard/DestinationCard.component.tsx
+++ components/DestinationCard/DestinationCard.component.tsx
@@ -12,13 +12,13 @@ import styles from "./DestinationCard.module.scss";
 export const DestinationCard: FC<DestinationCardProps> = ({ className, destination, ...props }) => {
   const { t } = useTranslation("destinations");
 
-  const { title, image } = destination;
+  const { name, image } = destination;
 
   return (
     <Card className={cn(styles.card, className)} {...props}>
-      <Image className={cn(styles.card__image)} src={image} alt={title} sizes="100%" fill priority />
+      <Image className={cn(styles.card__image)} src={image} alt={`Tour ${name}`} sizes="100%" fill priority />
 
-      <h3 className={cn(styles.card__title)}>{t(title)}</h3>
+      <h3 className={cn(styles.card__title)}>{t(name)}</h3>
     </Card>
   );
 };
