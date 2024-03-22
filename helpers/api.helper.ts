340e880351e345bc3f3a0326ba6e7c693d7f6dde
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-22 13:55:10 +0500

    fet: create api helper

---
+++ helpers/api.helper.ts
@@ -0,0 +1,10 @@
+export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
+
+export const API = {
+  tour: {
+    getList: `/tour/list/`,
+  },
+  destination: {
+    getList: `/destination/list/`,
+  },
+};
