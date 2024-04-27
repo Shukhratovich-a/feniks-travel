e469cf697261a54d260502470f5802786be9b0a9
shukhratovich-a <shukhratovich138@gmail.com>
2024-04-27 23:25:02 +0500

    refactor: connect tour modal

--- components/TourCard/TourCard.component.tsx
+++ components/TourCard/TourCard.component.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Image from "next/image";
 import { useTranslation } from "next-i18next";
 import cn from "classnames";
@@ -7,7 +7,7 @@ import { TourCardProps } from "./TourCard.props";
 
 import { DOMAIN } from "@/helpers/api.helper";
 
-import { Card, Rating } from "@/components";
+import { Card, Rating, TourModal } from "@/components";
 
 import { IconLocation } from "@/icons";
 
@@ -17,42 +17,48 @@ export const TourCard: FC<TourCardProps> = ({ className, tour, ...props }) => {
   const { image, title, destination, rating, days, nights, price } = tour;
   const { t } = useTranslation(["destinations"]);
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Card className={cn(styles.card, className)} {...props}>
-      <div className={cn(styles.card__image)}>
-        <Image src={`${DOMAIN}${image}`} alt={title} sizes="100%" fill priority />
-      </div>
+      <div onClick={() => setIsOpen(true)}>
+        <div className={cn(styles.card__image)}>
+          <Image src={`${DOMAIN}${image}`} alt={title} sizes="100%" fill priority />
+        </div>
 
-      <div className={cn(styles.card__content)}>
-        <span
-          className={cn(styles.card__destination)}
-          title={t(destination.name ? destination.name : destination.title)}
-        >
-          <IconLocation className={cn(styles.card__destination__icon)} />
-          <span className={cn(styles.card__destination__title)}>
-            {t(destination.name ? destination.name : destination.title)}
-          </span>
-        </span>
-
-        <h2 className={cn(styles.card__title)} title={title}>
-          {title}
-        </h2>
-
-        <span className={cn(styles.card__rating)}>
-          <Rating rating={rating} />
-          {rating}
-        </span>
-
-        <div className={cn(styles.card__content__bottom)}>
-          <span>
-            {`${days} ${t("common:days")}`} {nights && `${nights} ${t("common:nights")}`}
+        <div className={cn(styles.card__content)}>
+          <span
+            className={cn(styles.card__destination)}
+            title={t(destination.name ? destination.name : destination.title)}
+          >
+            <IconLocation className={cn(styles.card__destination__icon)} />
+            <span className={cn(styles.card__destination__title)}>
+              {t(destination.name ? destination.name : destination.title)}
+            </span>
           </span>
 
-          <span>
-            <strong>${price}</strong>
+          <h2 className={cn(styles.card__title)} title={title}>
+            {title}
+          </h2>
+
+          <span className={cn(styles.card__rating)}>
+            <Rating rating={rating} />
+            {rating}
           </span>
+
+          <div className={cn(styles.card__content__bottom)}>
+            <span>
+              {`${days} ${t("common:days")}`} {nights && `${nights} ${t("common:nights")}`}
+            </span>
+
+            <span>
+              <strong>{price}</strong>
+            </span>
+          </div>
         </div>
       </div>
+
+      <TourModal isOpen={isOpen} setIsOpen={setIsOpen} tour={tour} />
     </Card>
   );
 };
