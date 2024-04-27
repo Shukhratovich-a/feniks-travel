c34916b2978033325a38ece358ef8366bf655bbe
shukhratovich-a <shukhratovich138@gmail.com>
2024-04-27 23:19:13 +0500

    refactor: create apis for callback and book

--- api/tour.api.ts
+++ api/tour.api.ts
@@ -4,6 +4,7 @@ import axios from "./axios";
 
 import { ITour } from "@/interfaces/tour.interface";
 import { IGetListOptions } from "@/interfaces/request.interface";
+import { IBookTour } from "@/components/TourBookForm/TourBookForm.interface";
 
 import { API } from "@/helpers/api.helper";
 
@@ -11,4 +12,12 @@ export const getList = (options?: IGetListOptions) => {
   const query = queryString.stringifyUrl({ url: API.tour.getList, query: { ...options } });
 
   return axios.get<{ tours: ITour[] }>(query);
+};
+
+export const bookTour = (credentials: IBookTour) => {
+  return axios.post("/payme/pay-link/", { ...credentials });
+};
+
+export const callbackTour = (credentials: IBookTour) => {
+  return axios.post("/payme/callback/", { ...credentials });
 };
